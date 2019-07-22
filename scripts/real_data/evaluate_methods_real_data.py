0b1028cd12d884743bfbdc994ef9435a598b76c1
mikegloudemans <mgloud@durga.stanford.edu>
2019-07-21 21:50:44 -0700

    Combined real data to launch all methods together

--- scripts/real_data/evaluate_methods_real_data.py
+++ scripts/real_data/evaluate_methods_real_data.py
@@ -5,6 +5,10 @@ import sys
 import time
 import datetime
 import pandas as pd
+import traceback
+from multiprocessing import Pool
+import index_genes_for_rtc
+import os
 
 def main():
 
@@ -22,6 +26,10 @@ def main():
     subprocess.check_call("mkdir -p {0}".format(out_dir), shell=True)
 
     temp = json.loads(template)
+    
+    max_cores = 1
+    if "max_cores" in temp:
+        max_cores = int(temp["max_cores"])
 
     kept_data = []
     with open(temp["snp_list_file"]) as f:
@@ -37,93 +45,101 @@ def main():
 
     # Special loading phase for RTC...
     if "rtc" in temp["coloc_settings"]["methods"]:
-
-        # Load persistent data
-        pheno_file = "/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2016-01-15_v7/rna-seq/GTEx_Analysis_v7_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct.gz"
-        id_file = "/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2016-01-15_v7/sample_annotations/GTEx_Analysis_2016-01-15_v7_SampleAttributesDS.txt"
-        
-        # Load in map of IDs 
-        id_map = pd.read_csv(id_file, sep="\t")
-        id_map['SMTSD'] = id_map['SMTSD'].apply(lambda x: x.replace(" - ", "_")).apply(lambda x: x.replace(" ", "_")).apply(lambda x: x.replace("(", "")).apply(lambda x: x.replace(")", ""))
-            
-        pheno_map = pd.read_csv(pheno_file, sep="\t", skiprows=2)
-
+        index_genes_for_rtc.index_genes()
+           
     # Then for every locus in the "kept data"...
+
+    # Run GWAS SNPs separately just in case there happen to be any overlaps,
+    # which could lead to a race.
+    pool = Pool(max_cores)
     for i in range(len(kept_data)):
-
-        test = kept_data[i]
-        print test
-        
-        temp = json.loads(template)["coloc_settings"]
-        temp["snp_list_file"] = "/users/mgloud/projects/coloc_comparisons/tmp/snp_list{0}.txt".format(i)
-        temp["out_dir_group"] = out_dir
-
-        if "smr" in temp["methods"]:
-            # Needed for SMR
-            # NOTE: We're doing this for now because the new munged data have beta/se
-            # for some GWAS, but the old do not for any. Eventually, change this to make
-            # it run on the exact same set of files
-            test[2] = test[2].replace("munged", "munged/new")
-            test[4] = test[4].replace("munged", "munged/new")     
-   
-        # This file has issues I guess, figure this out later
-        if test[2].split("/")[-1] in ["GWAS_Adiponectin_Dastani_2012.txt.gz"]:
-            continue
-
-        if "Anthro" not in test[2]:
-            continue
-
- 
-        # Add locus to SNP list...but only once for each gene
-        with open("/users/mgloud/projects/coloc_comparisons/tmp/snp_list{0}.txt".format(i), "w") as w:
-            # Chrom / SNP / gene
-            w.write("{0}\t{1}\t{2}\n".format(test[0], test[1], test[7]))
-               
-        # Add corresponding gwas experiment to the list, if not already present
-        temp["gwas_experiments"][test[2]] = {"ref": "1kgenomes", "gwas_format": "pval_only", "N": "50000", "type":"quant"}
-        if test[2] != test[4]:
-            temp["gwas_experiments"][test[2]]["traits"] = [test[4]]
-
-        # Add corresponding eQTL tissue to the list
-        temp["eqtl_experiments"][test[3]] = {"ref": "1kgenomes", "gwas_format": "effect_size", "N": "500"}
-
-        # Prep for certain special cases for certain methods
-
-        if "twas" in temp["methods"]:
-            # Add corresponding eQTL tissue model
-            temp["eqtl_experiments"][test[3]]["model"] = {
-                    "pos":"/users/mgloud/projects/brain_gwas/scripts/data_prep/twas/Adipose_Visceral_Omentum.P01.pos", 
-                    "gene":"/users/mgloud/projects/brain_gwas/scripts/data_prep/twas/Adipose_Visceral_Omentum/Adipose_Visceral_Omentum.{0}.wgt.RDat", 
-                    "gene_dir_name": "Adipose_Visceral_Omentum"
-            }
-
-        if "rtc" in temp["methods"]:
-            temp["eqtl_experiments"][test[3]]["rtc_genos"] = "/users/mgloud/projects/coloc_comparisons/tmp/genos{0}.vcf.gz".format(i)
-            temp["eqtl_experiments"][test[3]]["rtc_phenos"] = "/users/mgloud/projects/coloc_comparisons/tmp/phenos{0}.bed.gz".format(i)
-
-            prep_rtc_input(id_map, pheno_map, test, i)
-
-        # Write config file to the appropriate directory
-        with open("/users/mgloud/projects/coloc_comparisons/tmp/coloc_comparisons{0}.config".format(i), "w") as w:
-            json.dump(temp, w)
-
-        # TODO: Turn this into not a BG but a foreground task, then have it called by a multi-threading pool
-        # Run the test
-        subprocess.call("python /users/mgloud/projects/brain_gwas/scripts/dispatch.py /users/mgloud/projects/coloc_comparisons/tmp/coloc_comparisons{0}.config 1".format(i), shell=True)
+        pool.apply_async(launch_pipeline_wrapper, args=(template, kept_data, out_dir,  i))
+    pool.close()
+    pool.join()
 
     # Purge tmp files
     subprocess.call("rm -rf /users/mgloud/projects/coloc_comparisons/tmp/*", shell=True)
 
-def prep_rtc_input(id_map, pheno_map, test, i):
+
+def launch_pipeline_wrapper(template, kept_data, out_dir, i):
+    try:
+        launch_pipeline(template, kept_data, out_dir, i)
+    except Exception as e:
+        traceback.print_exc(file=sys.stdout)
+        raise Exception("Failed full pipeline run on real data.")
+
+
+def launch_pipeline(template, kept_data, out_dir, i):
+
+    test = kept_data[i]
+    print test
+    
+    temp = json.loads(template)["coloc_settings"]
+    temp["snp_list_file"] = "/users/mgloud/projects/coloc_comparisons/tmp/snp_list{0}.txt".format(i)
+    temp["out_dir_group"] = out_dir
+
+    if "smr" in temp["methods"]:
+        # Needed for SMR
+        # NOTE: We're doing this for now because the new munged data have beta/se
+        # for some GWAS, but the old do not for any. Eventually, change this to make
+        # it run on the exact same set of files
+        test[2] = test[2].replace("munged", "munged/new")
+        test[4] = test[4].replace("munged", "munged/new")     
+
+    # This file has issues I guess, figure this out later
+    if test[2].split("/")[-1] in ["GWAS_Adiponectin_Dastani_2012.txt.gz"]:
+        return
+
+    # Add locus to SNP list...but only once for each gene
+    with open("/users/mgloud/projects/coloc_comparisons/tmp/snp_list{0}.txt".format(i), "w") as w:
+        # Chrom / SNP / gene
+        w.write("{0}\t{1}\t{2}\n".format(test[0], test[1], test[7]))
+           
+    # Add corresponding gwas experiment to the list, if not already present
+    temp["gwas_experiments"][test[2]] = {"ref": "1kgenomes", "gwas_format": "pval_only", "N": "50000", "type":"quant"}
+    if test[2] != test[4]:
+        temp["gwas_experiments"][test[2]]["traits"] = [test[4]]
+
+    # Add corresponding eQTL tissue to the list
+    temp["eqtl_experiments"][test[3]] = {"ref": "1kgenomes", "gwas_format": "effect_size", "N": "500"}
+
+    # Prep for certain special cases for certain methods
+
+    if "twas" in temp["methods"]:
+        # Add corresponding eQTL tissue model
+        temp["eqtl_experiments"][test[3]]["model"] = {
+                "pos":"/users/mgloud/projects/brain_gwas/scripts/data_prep/twas/Adipose_Visceral_Omentum.P01.pos", 
+                "gene":"/users/mgloud/projects/brain_gwas/scripts/data_prep/twas/Adipose_Visceral_Omentum/Adipose_Visceral_Omentum.{0}.wgt.RDat", 
+                "gene_dir_name": "Adipose_Visceral_Omentum"
+        }
+
+    if "rtc" in temp["methods"]:
+        temp["eqtl_experiments"][test[3]]["rtc_genos"] = "/users/mgloud/projects/coloc_comparisons/tmp/genos{0}.vcf.gz".format(i)
+        temp["eqtl_experiments"][test[3]]["rtc_phenos"] = "/users/mgloud/projects/coloc_comparisons/tmp/phenos{0}.bed.gz".format(i)
+
+        prep_rtc_input(test, i)
+
+    # Write config file to the appropriate directory
+    with open("/users/mgloud/projects/coloc_comparisons/tmp/coloc_comparisons{0}.config".format(i), "w") as w:
+        json.dump(temp, w)
+
+    # Run the test
+    subprocess.call("python /users/mgloud/projects/brain_gwas/scripts/dispatch.py /users/mgloud/projects/coloc_comparisons/tmp/coloc_comparisons{0}.config 1".format(i), shell=True)
+
+def prep_rtc_input(test, i, rtc_index_dir = "/users/mgloud/projects/coloc_comparisons/data/rtc-gene-index"):
         # Do preprocessing to prepare the additional input for RTC! (Note: eventually
         # we should make this into a separate optional script, where the required input
         # files are specified and then the script pulls out the necessary subset of info)
        
         geno_file = "/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2016-01-15_v7/genotypes/WGS/variant_calls/GTEx_Analysis_2016-01-15_v7_WholeGenomeSeq_652Ind_GATK_HaplotypeCaller.vcf.gz"
 
+        # Load in map of IDs
+        id_file = "/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2016-01-15_v7/sample_annotations/GTEx_Analysis_2016-01-15_v7_SampleAttributesDS.txt"
+        id_map = pd.read_csv(id_file, sep="\t")
+        id_map['SMTSD'] = id_map['SMTSD'].apply(lambda x: x.replace(" - ", "_")).apply(lambda x: x.replace(" ", "_")).apply(lambda x: x.replace("(", "")).apply(lambda x: x.replace(")", ""))
+
         # Figure out which tissue we're going for
         tissue = test[3].split("/")[-1].split(".")[0]
-
        
         # Get all samples corresponding to our tissue of interest
         id_map = id_map[(id_map['SMTSD'] == tissue).values]
@@ -131,20 +147,32 @@ def prep_rtc_input(id_map, pheno_map, test, i):
 
         # Subset genotype file down to region of interest
         # Header
-        subprocess.check_call("zcat {0} 2> /dev/null | head -n 10000 | grep \#CHROM > /users/mgloud/projects/coloc_comparisons/tmp/genotype_vcf{1}.tmp".format(geno_file, i), shell=True)
-        subprocess.check_call("zcat {0} 2> /dev/null | head -n 10000 | grep \## > /users/mgloud/projects/coloc_comparisons/tmp/genos{1}.vcf".format(geno_file, i), shell=True)
+        subprocess.check_call("zcat {0} 2> /dev/null | head -n 2000 | grep \#CHROM > /users/mgloud/projects/coloc_comparisons/tmp/genotype_vcf{1}.tmp".format(geno_file, i), shell=True)
+        subprocess.check_call("zcat {0} 2> /dev/null | head -n 2000 | grep \## > /users/mgloud/projects/coloc_comparisons/tmp/genos{1}.vcf".format(geno_file, i), shell=True)
         # Everything else we need
         subprocess.check_call('''tabix {0} {1}:{2}-{3} | awk '{{split($3, a, "_");  $3=a[1] "_" a[2]; print $0}}' | sed s/\\ /\\\\t/g >> /users/mgloud/projects/coloc_comparisons/tmp/genotype_vcf{4}.tmp'''.format(geno_file, test[0], int(test[1]) - 1000000, int(test[1]) + 1000000, i), shell=True)
-
 
         # Load subsetted genotype (No filtering is actually needed for the
         # individuals in the genotype matrix, because QTLtools performs this filtering)
         genotypes = pd.read_csv("/users/mgloud/projects/coloc_comparisons/tmp/genotype_vcf{0}.tmp".format(i), sep="\t")
         genotypes["#CHROM"] = genotypes["#CHROM"].apply(lambda x: "chr" + str(x))
 
-        # Filter expression matrix down to a single gene of interest
-        # and to samples we want
-        pheno_sub = pheno_map[pheno_map['Name'] == test[7]]
+        # Get expression matrix for the gene we want
+        with open("{0}/header.txt".format(rtc_index_dir)) as f:
+            header = f.readline().strip().split()
+        gene = test[7].replace("ENSG00000", "")
+        g1 = gene[:2]
+        g2 = gene[2:4]
+       
+        print os.listdir("{0}/{1}/{2}".format(rtc_index_dir, g1, g2))
+        print gene
+        if "{0}.txt".format(gene) not in os.listdir("{0}/{1}/{2}".format(rtc_index_dir, g1, g2)):
+            # Eventually might want to actually log this to a file
+            return "Gene not found in RTC table"
+        pheno_sub = pd.read_csv("{0}/{1}/{2}/{3}.txt".format(rtc_index_dir, g1, g2, gene), sep="\t", header=None, names = header)
+        print pheno_sub.head()
+
+        #pheno_sub = pheno_map[pheno_map['Name'] == test[7]]
         tissue_subset = ["Name"] + [e for e in list(pheno_sub.columns.values) if e in tissue_samples]
         expression = pheno_sub[tissue_subset]
 
--- scripts/real_data/index_genes_for_rtc.py
+++ scripts/real_data/index_genes_for_rtc.py
@@ -1,22 +1,41 @@
 import subprocess
 import gzip
+import os
 
-# Load persistent data
-pheno_file = "/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2016-01-15_v7/rna-seq/GTEx_Analysis_v7_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct.gz"
-rtc_index_dir = "/users/mgloud/projects/coloc_comparisons/data/rtc-gene-index"
+# RTC requires that we input gene expression. Here we provide a convenient indexed directory structure
+# so that the lookups only take a fraction of the time
 
-with gzip.open(pheno_file) as f:
-    f.readline()
-    f.readline()
+def index_genes(rtc_index_dir = "/users/mgloud/projects/coloc_comparisons/data/rtc-gene-index"):
+
+    rtc_index_dir = "/users/mgloud/projects/coloc_comparisons/data/rtc-gene-index"
+
+    # Index genes if not already indexed.
+    if "header.txt" in os.listdir(rtc_index_dir):
+        return "Already indexed"
+
+
+    # Load persistent data
+    pheno_file = "/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2016-01-15_v7/rna-seq/GTEx_Analysis_v7_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct.gz"
+
+    with gzip.open(pheno_file) as f:
+        f.readline()
+        f.readline()
+        header = f.readline()   # Load header; write it later to indicate we're finished
+        for line in f:
+            gene = line.strip().split()[0].replace("ENSG00000", "")
+            g1 = gene[:2]
+            g2 = gene[2:4]
+            subprocess.check_call("mkdir -p {0}/{1}/{2}".format(rtc_index_dir, g1, g2), shell=True)
+
+            with open("{0}/{1}/{2}/{3}.txt".format(rtc_index_dir, g1, g2, gene), "w") as w:
+                w.write(line)
+
+    # Since we check for the header to see if indexing is done,
+    # don't write the header until the indexing is completed.
+    # Otherwise if the program is interrupted, we'll never finish
     with open("{0}/header.txt".format(rtc_index_dir), "w") as w:
-        w.write(f.readline())
+        w.write(header)
 
-    for line in f:
-        gene = line.strip().split()[0].replace("ENSG00000", "")
-        g1 = gene[:2]
-        g2 = gene[2:4]
-        subprocess.check_call("mkdir -p {0}/{1}/{2}".format(rtc_index_dir, g1, g2), shell=True)
 
-        with open("{0}/{1}/{2}/{3}.txt".format(rtc_index_dir, g1, g2, gene), "w") as w:
-            w.write(line)
-
+if __name__ == "__main__":
+    index_genes()
